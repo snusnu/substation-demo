49440d50034042af20a8dda1e2bf174d20c4568f
snusnu <gamsnjaga@gmail.com>
2013-05-28 15:21:59 +0200

    Nuke leftover code showing method defined by anima

--- app/demo/models/person.rb
+++ app/demo/models/person.rb
@@ -4,10 +4,6 @@ class Demo
 
     class Person
       include Anima.new(:id, :name)
-
-      def self.attributes_hash(instance)
-        { :id => instance.id, :name => instance.name }
-      end
     end
   end
 end
